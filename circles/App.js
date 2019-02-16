fe444ab57f45a407407bc1a16a62c144a602859c
sarahnash <sarahnash1112@gmail.com>
2019-02-16 12:11:09 -0600

    circles and nametags complete

--- circles/App.js
+++ circles/App.js
@@ -1,9 +1,21 @@
 // Render the circles using React!
-const App = (props) => {
-    let { circles } = props;
-    return (
-        <span>circles</span>
-    )
+const App = ({ circles }) => {
+    // let { circles } = props;
+    console.log(circles)
+    return(
+        <div className="d-flex flex-column align-items-center">
+            {
+                circles.map(function (circle, index) {
+                    console.log(index)
+                    return <div style={{
+                        width: (circle.radius * 2), 
+                        height: (circle.radius * 2), 
+                        borderRadius: circle.radius, 
+                        backgroundColor: circle.color
+                    }}
+                    key={index}></div>
+            })
+        }
+        </div>
+    ) 
 }
-
-const Circle = null;  // CHALLENGE: Write a separate Circle component for use in the App component
--- nameTags/App.js
+++ nameTags/App.js
@@ -1,8 +1,19 @@
 // Render the name tags using React!
-const App = (props) => {
-    let { nameTags } = props;
+const App = ({nameTags}) => {
+    // let { nameTags } = props;
     return (
-        <span>name tags</span>
+        <div className="d-flex flex-column align-items-center">
+            {
+                nameTags.map(function ( nameTag, index) {
+                    return (
+                    <div className="nametag mx-auto w-50 text-center border m-1" key={index}>
+                        <div className="hello bg-primary text-white p-3 h3">Hello, my name is:</div>
+                        <div className="name h4 p-5">{nameTag}</div>
+                    </div>
+                    )
+            })
+        }
+        </div>
     )
 }
 
